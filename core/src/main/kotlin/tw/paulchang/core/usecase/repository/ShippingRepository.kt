80d2fb6c4509c34c800942e917d1c763385a28f8
Paul Chang <a2323269@gmail.com>
2021-04-08 16:20:31 +0800

    feat: define shipping related entity, usecase & request dto

---
+++ core/src/main/kotlin/tw/paulchang/core/usecase/repository/ShippingRepository.kt
@@ -0,0 +1,5 @@
+package tw.paulchang.core.usecase.repository
+
+import tw.paulchang.core.usecase.shipping.DispatchShippingUseCase
+
+interface ShippingRepository : DispatchShippingUseCase.ShippingRepository
