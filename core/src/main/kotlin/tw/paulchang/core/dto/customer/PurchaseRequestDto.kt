5dd03792c45bae003dece97e32f091058a820c53
Paul Chang <a2323269@gmail.com>
2021-04-09 15:18:36 +0800

    chore: added payment type to the message for purchase topic

--- core/src/main/kotlin/tw/paulchang/core/dto/customer/PurchaseRequestDto.kt
+++ core/src/main/kotlin/tw/paulchang/core/dto/customer/PurchaseRequestDto.kt
@@ -3,4 +3,5 @@ package tw.paulchang.core.dto.customer
 data class PurchaseRequestDto(
     val customerId: Long,
     val cartIds: List<Long>,
+    val paymentType: String,
 )
--- customer-service/src/main/kotlin/tw/paulchang/customerservice/dto/PurchaseTopicModel.kt
+++ customer-service/src/main/kotlin/tw/paulchang/customerservice/dto/PurchaseTopicModel.kt
@@ -2,5 +2,6 @@ package tw.paulchang.customerservice.dto
 
 data class PurchaseTopicModel(
     val customerId: Long,
-    val purchasedProductsWithAmount: Map<Long, Int>
+    val purchasedProductsWithAmount: Map<Long, Int>,
+    val paymentType: String
 )
