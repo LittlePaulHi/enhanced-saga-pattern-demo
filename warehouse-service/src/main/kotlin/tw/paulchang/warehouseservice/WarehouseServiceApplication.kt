050499df08c0c7f66a82190e248739e81b783143
Paul Chang <a2323269@gmail.com>
2021-04-23 21:21:57 +0800

    feat: added Redis dependency and related configs for warehouse-service

--- warehouse-service/src/main/kotlin/tw/paulchang/warehouseservice/WarehouseServiceApplication.kt
+++ warehouse-service/src/main/kotlin/tw/paulchang/warehouseservice/WarehouseServiceApplication.kt
@@ -3,9 +3,11 @@ package tw.paulchang.warehouseservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @SpringBootApplication
 @EnableR2dbcRepositories
+@EnableRedisRepositories
 class WarehouseServiceApplication
 
 fun main(args: Array<String>) {
