54a6befafa8229fbb0abf82436ce0c325fd85e95
Paul Chang <a2323269@gmail.com>
2021-04-25 00:20:01 +0800

    feat: implemented compensate consumer for warehouse' fetchGoods

--- core/src/main/kotlin/tw/paulchang/core/model/KafkaTopics.kt
+++ core/src/main/kotlin/tw/paulchang/core/model/KafkaTopics.kt
@@ -2,5 +2,6 @@ package tw.paulchang.core.model
 
 object KafkaTopics {
     const val PURCHASE = "purchase"
+    const val COMPENSATE_WAREHOUSE_GOODS = "compensateWarehouseGoods"
     const val FINISHED_BUY_EVENT = "finishedBuyEvent"
 }
