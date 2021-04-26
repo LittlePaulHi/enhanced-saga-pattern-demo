b9e6fb8f183bdbcdb41471e61507e68fc2964c80
Paul Chang <a2323269@gmail.com>
2021-04-26 22:26:40 +0800

    feat: added another REST API to purchase with quota-cache

--- core/src/main/kotlin/tw/paulchang/core/model/KafkaTopics.kt
+++ core/src/main/kotlin/tw/paulchang/core/model/KafkaTopics.kt
@@ -2,6 +2,7 @@ package tw.paulchang.core.model
 
 object KafkaTopics {
     const val PURCHASE = "purchase"
+    const val PURCHASE_VIA_QUOTA_CACHE = "purchaseViaQuotaCache"
     const val COMPENSATE_WAREHOUSE_GOODS = "compensateWarehouseGoods"
     const val COMPENSATE_PAYMENT = "compensatePayment"
     const val FINISHED_BUY_EVENT = "finishedBuyEvent"
--- customer-service/src/main/kotlin/tw/paulchang/customerservice/controller/KafkaProducerController.kt
+++ customer-service/src/main/kotlin/tw/paulchang/customerservice/controller/KafkaProducerController.kt
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import tw.paulchang.core.dto.customer.PurchaseRequestDto
+import tw.paulchang.core.model.KafkaTopics
 import tw.paulchang.core.model.PurchaseTopicModel
 import tw.paulchang.customerservice.database.dao.CartDao
 import tw.paulchang.customerservice.database.model.CartModel
@@ -35,6 +36,30 @@ class KafkaProducerController(
             .flatMapMaybe { it: MutableList<CartModel>? ->
                 return@flatMapMaybe it?.let { carts ->
                     producerService.sendPurchaseRecordToKafka(
+                        KafkaTopics.PURCHASE,
+                        PurchaseTopicModel(
+                            customerId = purchaseRequestDto.customerId,
+                            cartIds = purchaseRequestDto.cartIds,
+                            purchasedProductsWithAmount = carts.associate { it.productId to it.quantity },
+                            paymentType = purchaseRequestDto.paymentType,
+                            shippingType = purchaseRequestDto.shippingType
+                        )
+                    )
+                } ?: Maybe.empty()
+            }
+    }
+
+    @PostMapping("/quota-cache/purchase")
+    fun purchaseViaQuotaCache(
+        @RequestBody purchaseRequestDto: PurchaseRequestDto
+    ): Maybe<ListenableFuture<SendResult<String, PurchaseTopicModel>>> {
+        return cartDao.getAllCartsByIds(purchaseRequestDto.cartIds)
+            .observeOn(scheduler) // switch from db's thread
+            .toList()
+            .flatMapMaybe { it: MutableList<CartModel>? ->
+                return@flatMapMaybe it?.let { carts ->
+                    producerService.sendPurchaseRecordToKafka(
+                        KafkaTopics.PURCHASE_VIA_QUOTA_CACHE,
                         PurchaseTopicModel(
                             customerId = purchaseRequestDto.customerId,
                             cartIds = purchaseRequestDto.cartIds,
